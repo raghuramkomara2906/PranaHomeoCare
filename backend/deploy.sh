2227f68ab5d91216ff13aa65d33e6d93e318bbc6
Raghu Ram Komara <raghuramkomara2906@gmail.com>
2026-07-28 23:25:34 -0400

    dple

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
+cd /app
+echo "==> DATABASE_URL starts with: $(echo $DATABASE_URL | cut -c1-30)..."
 echo "==> Running migrations"
 alembic upgrade head
 echo "==> Seeding database"
